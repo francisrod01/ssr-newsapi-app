02d6966177d53e22a524ce158886b7f4602e58a1
Francis Rodrigues <francis.rod01@outlook.com>
2020-07-26 18:39:34 -0300

    Updates

---
+++ precache-manifest.b0e5e9faf1f1d65e331ce75bf50a3efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c197f9bdb055499a2667fccb23881e1c",
+    "url": "/ssr-newsapi-app/index.html"
+  },
+  {
+    "revision": "7219ccecca5b5807e127",
+    "url": "/ssr-newsapi-app/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "42c40a22bf8de3ef1041",
+    "url": "/ssr-newsapi-app/static/js/2.9488c6f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ssr-newsapi-app/static/js/2.9488c6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7219ccecca5b5807e127",
+    "url": "/ssr-newsapi-app/static/js/main.7cba220b.chunk.js"
+  },
+  {
+    "revision": "69ef9db6b28945ff604e",
+    "url": "/ssr-newsapi-app/static/js/runtime-main.ed4154b5.js"
+  }
+]);
